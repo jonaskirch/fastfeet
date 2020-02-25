34642e6c5d22bd6a9fb62fde242782dffc2bbec6
Jonas Kirch <jonaskirch9@gmail.com>
2020-02-25 18:42:46 -0300

    add pages register and routes

---
+++ frontend/src/pages/DeliverymanRegister/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function DeliverymanRegister() {
+  return <Container>Deliveryman Register</Container>;
+}
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,9 +3,12 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
-import Delivery from '~/pages/Delivery/Query';
-import Deliveryman from '~/pages/Deliveryman/Query';
-import Recipient from '~/pages/Recipient/Query';
+import DeliveryRegister from '~/pages/DeliveryRegister';
+import Delivery from '~/pages/Delivery';
+import Deliveryman from '~/pages/Deliveryman';
+import DeliverymanRegister from '~/pages/DeliverymanRegister';
+import RecipientRegister from '~/pages/RecipientRegister';
+import Recipient from '~/pages/Recipient';
 import Problem from '~/pages/Problem';
 
 export default function Routes() {
@@ -14,8 +17,15 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       {/* <Route path="/register" component={SignUp} /> */}
 
+      <Route path="/delivery/:id" component={DeliveryRegister} isPrivate />
       <Route path="/delivery" component={Delivery} isPrivate />
+      <Route
+        path="/deliveryman/:id"
+        component={DeliverymanRegister}
+        isPrivate
+      />
       <Route path="/deliveryman" component={Deliveryman} isPrivate />
+      <Route path="/recipient/:id" component={RecipientRegister} isPrivate />
       <Route path="/recipient" component={Recipient} isPrivate />
       <Route path="/problem" component={Problem} isPrivate />
     </Switch>
