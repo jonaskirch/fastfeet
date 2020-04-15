10c17bc5336fdc8628847e40bce0aedfc5e8ecad
Jonas Kirch <jonaskirch9@gmail.com>
2020-04-15 19:47:37 -0300

    fix static path

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,17 +27,19 @@ class App {
   }
 
   routes() {
-    this.server.use('/api',routes);
+    this.server.use('/api', routes);
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'temp', 'uploads'))
     );
     this.server.use(
       '/static',
-      express.static(pathJoin(__dirname, '..','public','static'))
+      express.static(path.resolve(__dirname, '..', 'public', 'static'))
     );
     this.server.use(express.static(path.resolve(__dirname, '..', 'public')));
-    this.server.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html')));
+    this.server.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
+    );
     this.server.use(Sentry.Handlers.errorHandler());
   }
 
