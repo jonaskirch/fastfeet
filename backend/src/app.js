33043b1e80c07f01156eb33e2a13aaefd80d60ac
Jonas Kirch <jonaskirch9@gmail.com>
2020-04-15 22:00:22 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
-import path, { join as pathJoin } from 'path';
+import path from 'path';
 import cors from 'cors';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import sentryConfig from './config/sentry';
 import 'express-async-errors';
-import staticConfig from './config/static';
 import routes from './routes';
 import './database';
 
@@ -16,37 +15,9 @@ class App {
 
     Sentry.init(sentryConfig);
 
-    this.static();
     this.middlewares();
     this.routes();
-    this.rewriteIndex();
     this.exceptionHandler();
-  }
-
-  static() {
-    this.server.use(
-      '/files',
-      express.static(path.resolve(__dirname, '..', 'temp', 'uploads'))
-    );
-
-    this.server.use(
-      '/static',
-      express.static(pathJoin(__dirname, 'public/static'), staticConfig.static)
-    );
-    this.server.use(
-      express.static(pathJoin(__dirname, 'public'), staticConfig.public)
-    );
-
-    // this.server.use(
-    //   '/static',
-    //   express.static(
-    //     pathJoin(__dirname, 'public', 'static'),
-    //     staticConfig.static
-    //   )
-    // );
-    // this.server.use(
-    //   express.static(pathJoin(__dirname, 'public'), staticConfig.public)
-    // );
   }
 
   middlewares() {
@@ -57,36 +28,19 @@ class App {
 
   routes() {
     this.server.use('/api', routes);
-    // this.server.use(
-    //   '/files',
-    //   express.static(path.resolve(__dirname, '..', 'temp', 'uploads'))
-    // );
-    // this.server.use(
-    //   '/static',
-    //   express.static(path.resolve(__dirname, '..', 'public', 'static'))
-    // );
-    // this.server.use(express.static(path.resolve(__dirname, '..', 'public')));
-    // this.server.get('*', (req, res) =>
-    //   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
-    // );
-    this.server.use(Sentry.Handlers.errorHandler());
-  }
-
-  rewriteIndex() {
-    // if (process.env.NODE_ENV === 'development') {
-    //   this.server.get('/*', (req, res) =>
-    //     res.redirect(process.env.APP_FRONTEND_URL)
-    //   );
-    //   return;
-    // }
-    this.server.get(
-      '/*',
-      (req, res, next) => {
-        req.url = '/';
-        next();
-      },
-      express.static(pathJoin(__dirname, 'public'), staticConfig.public)
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'temp', 'uploads'))
     );
+    this.server.use(
+      '/static',
+      express.static(path.resolve(__dirname, '..', 'public', 'static'))
+    );
+    this.server.use(express.static(path.resolve(__dirname, '..', 'public')));
+    this.server.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'))
+    );
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   exceptionHandler() {
